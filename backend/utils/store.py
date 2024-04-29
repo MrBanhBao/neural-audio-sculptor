00f0a1bfd39f2dd43c5d0fd3d51b3f83d5bc22d6
Hao <hao@pop-os.localdomain>
2024-04-29 20:26:18 +0200

    new reference for config.yaml

--- backend/utils/store.py
+++ backend/utils/store.py
@@ -1,6 +1,6 @@
 from data_models import Config, Transform2DArgs, Transform3DArgs
 
-CONFIG_FILE = "../config.yaml"
+CONFIG_FILE = "../frontend/static/config.yaml"
 
 config: Config = Config.load(config_file=CONFIG_FILE)
 
