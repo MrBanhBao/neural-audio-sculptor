e5727db281e34bb3e7c99a90ff640507fd9f5f1e
Hao <hao@pop-os.localdomain>
2024-05-11 12:49:50 +0200

    removed stores

--- frontend/src/lib/stores/store.ts
+++ frontend/src/lib/stores/store.ts
@@ -5,7 +5,3 @@ export const audioMetaData = writable({} as AudioMetaData);
 export const currentFrame = writable(0)
 export const isPlaying = writable(false);
 export const statusFeedback = writable({ status: "idle", message: "" } as StatusFeedback);
-
-export const audioFileName = writable('');
-export const audioTracks = writable();
-export const featureTrack = writable();
